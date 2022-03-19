b40a7fc332a6b3b8a1c6bd9354687eb2bb08cc22
prince-111 <princesd991@gmail.com>
2022-03-19 23:46:30 +0530

    intial commit

---
+++ OOPS/Methods/Pridefinedmethods.java
@@ -0,0 +1,7 @@
+import java.lang.*;
+
+public class Pridefinedmethods {
+    public static void main(String[] args){
+        System.out.println("The maximumnumber is: " + Math.max(7,4) );
+    }
+}
