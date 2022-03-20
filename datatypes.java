b510eba9cc6210a96d7bddf39fe54af571a46cf8
prince-111 <princesd991@gmail.com>
2022-03-20 23:28:04 +0530

    dataypes

---
+++ datatypes.java
@@ -0,0 +1,16 @@
+public class datatypes {
+    public static void main(String[] arg){
+        int mynum=5;   // integer (whole number)
+        float myfloat=1.2f; // floating point number
+        char mylatter= 'P';  // character
+        boolean mybool = true;  //boolean
+        String mytext = "hello"; //string
+
+        System.out.println(mynum);
+        System.out.println(myfloat);
+        System.out.println(mylatter);
+        System.out.println(mytext);
+        System.out.println(mybool);
+
+    }
+}
