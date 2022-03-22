64f540f93cf6dee2fe6138bbe8381f9c9a0e3ff4
prince-111 <princesd991@gmail.com>
2022-03-22 17:14:34 +0530

    Arithmetic operation

---
+++ operators/Arithmeticoperator.java
@@ -0,0 +1,13 @@
+public class Arithmeticoperator {
+    
+    public static void main(String[] args){
+        int a=15;
+        int b=10;
+        
+        System.out.println(a+b); //25
+        System.out.println(a-b); //5
+        System.out.println(a*b); //150
+        System.out.println(a/b); //1
+        System.out.println(a%b); //5
+    }
+}
