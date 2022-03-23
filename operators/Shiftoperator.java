e3a2e4e4d6ecc3a55ad7906d0d5418ecb27b2bc6
prince-111 <princesd991@gmail.com>
2022-03-23 21:39:11 +0530

    shift operation

---
+++ operators/Shiftoperator.java
@@ -0,0 +1,16 @@
+public class Shiftoperator {
+    public static void main(String args[]){
+        // "<<" left shift operator.
+        // System.out.println(10<<2); // 10*2^2=10*4=40
+        // System.out.println(10<<3); // 10*2^3=10*8=80
+
+        // ">>" right shift operator.
+        //    System.out.println(10>>2); // 10/2^2=10/4=2.
+        //    System.out.println(20>>2); // 20/2^2=20/4=5.
+         
+        // ">> vs >>>"
+        System.out.println(20>>2); //5.
+        System.out.println(20>>>3); //2.
+
+    }
+}
