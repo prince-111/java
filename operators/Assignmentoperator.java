875a94c68596cd747b5f2ea28e6439d1aad86e9c
prince-111 <princesd991@gmail.com>
2022-03-23 21:37:24 +0530

    assignmnent operation

---
+++ operators/Assignmentoperator.java
@@ -0,0 +1,17 @@
+public class Assignmentoperator {
+    public static void main(String arg[]){
+        int a=15;
+        int b=25;
+        // a+=5; // a=a+5 (a=15+5)
+        // b-=20; // b=b-20 (b=25-20)
+        a*=3;  // a=a*3 (a=15*3)
+        b/=5; // b=b/5 (b=25/5)
+
+
+        System.out.println(a);
+        System.out.println(b);
+
+
+
+    }
+}
