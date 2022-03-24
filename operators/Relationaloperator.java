93324557dadb629d439cf0fb818869b0192c1091
prince-111 <princesd991@gmail.com>
2022-03-24 22:11:40 +0530

    Relational operator

---
+++ operators/Relationaloperator.java
@@ -0,0 +1,21 @@
+class Relationaloperator{
+    public static void main(String[] args ){
+        // create variable 
+        int a=6, b=12;
+
+        // value of a and b
+        System.out.println("a is " + a + "and b is " + b);
+        // == operator
+        System.out.println(a==b);  //false
+        // != operator
+        System.out.println(a!=b); //true
+        // > operator
+        System.out.println(a>b); // false
+        // < operator
+        System.out.println(a<b); // true
+        // >= operator
+        System.out.println(a>=b); // false
+        // <= operator
+        System.out.println(a<=b); // true
+    }
+}
